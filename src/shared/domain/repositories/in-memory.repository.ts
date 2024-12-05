e405fccede26208215f6c7c28eeaede8886b9eca
Felipe <fdamasio17@gmail.com>
2024-12-05 18:11:12 -0300

    Adding repository InMemoryRepository - part 2

--- src/shared/domain/repositories/in-memory.repository.ts
+++ src/shared/domain/repositories/in-memory.repository.ts
@@ -1,27 +1,52 @@
 import { Entity } from '../entites/entity'
+import { NotFoundError } from '../errors/not-found-error'
 import { RepositoryInterface } from './repository-contracts'
 
 export abstract class InMemoryRepository<E extends Entity>
   implements RepositoryInterface<E>
 {
+  items: E[] = []
+
   async findAll(): Promise<E[]> {
     return this.items
   }
   async findById(id: string): Promise<E> {
-    const _id = `${id}`
-    const entity = this.items.find(() => entity.id === _id)
-    if (!entity) throw new Error('Entity not found')
-
-    return entity
+    return this._get(id)
   }
   async insert(entity: E): Promise<void> {
     this.items.push(entity)
   }
-  update(entity: E): Promise<void> {
-    throw new Error('Method not implemented.')
+  async update(entity: E): Promise<void> {
+    await this._get(entity.id)
+
+    const index = await this._getIndex(entity.id)
+
+    this.items[index] = entity
   }
-  delete(id: string): Promise<void> {
-    throw new Error('Method not implemented.')
+
+  async delete(id: string): Promise<void> {
+    await this._get(id)
+
+    const index = await this._getIndex(id)
+
+    this.items.splice(index, 1)
   }
-  items: E[] = []
+
+  protected async _get(id: string): Promise<E> {
+    const _id = `${id}`
+    const entity = this.items.find(() => entity.id === _id)
+
+    if (!entity) throw new NotFoundError('Entity not found')
+
+    return entity
+  }
+
+  protected async _getIndex(id: string): Promise<number> {
+    const _id = `${id}`
+    const entityId = this.items.findIndex(() => id === _id)
+
+    if (!entityId) throw new NotFoundError('Entity not found')
+
+    return entityId
+  }
 }
